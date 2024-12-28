99041b6f46986e7beeeebcdc07aab7991700240e
Steve Dickinson <dsdickinson@gmail.com>
2024-12-28 16:11:32 +0000

    Adding debugging, and initial triton server and MQTT bits.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use opencv::{
     highgui, 
-    imgcodecs::{imencode, ImwriteFlags},
+    imgcodecs::{imencode},
     prelude::*, 
     videoio, 
     core::Vector,
@@ -19,14 +19,16 @@ struct Args {
 
 fn main() -> Result<()> {
 
-/*
-#    let args: Vec<String> = env::args().collect();
-#    let args = Args::parse();
-#    let query = &args[1];
-#    let video_source = &args[2];
+    let debug = 0;
 
-#	println!("Searching for {query}");
-#	println!("Video source {file_path}");
+/*
+    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
+    let query = &args[1];
+    let video_source = &args[2];
+
+    println!("Searching for {query}");
+    println!("Video source {file_path}");
 */
 
 /*
@@ -43,15 +45,92 @@ fn main() -> Result<()> {
 */
 
 /*
- * https://github.com/octoml/triton-client-rs
-// un-auth'd use of Triton
-let client = Client::new("http://localhost:8001/", None).await?;
-let models = client
-    .repository_index(triton_client::inference::RepositoryIndexRequest {
-        repository_name: "".into(), // This should show us models not referenced by repo name.
-        ready: false,               // show all models, not just ready ones.
-    })
-    .await?;
+    // Set some immutable variables
+    // Triton Server
+    let triton_server_ip    = "localhost";
+    let triton_server_port  = 8001;
+    let triton_server_url   = format!("http://{}:{}", triton_server_ip, triton_server_port);
+
+    // MQTT broker
+    let mqtt_broker_ip      = "localhost";
+    let mqtt_broker_port    = 1883;
+    let mqtt_broker_url     = format!("mqtt://{}:{}", mqtt_broker_ip, mqtt_broker_port);
+    let mqtt_broker_user    = "";
+
+    // MQTT channels
+    //mqtt_topic_raw          = "gg/video/raw/" + location_code + "/" + stream
+    //mqtt_topic_infer        = "gg/video/inf/" + location_code + "/" + stream
+    //mqtt_topic_infer_boxes  = "gg/boxes/inf/" + location_code + "/" + stream
+
+    // Redis
+    let redis_host = "";
+    let redis_port = "";
+*/
+
+/*
+    // https://github.com/octoml/triton-client-rs
+    // un-auth'd use of Triton
+    let client = Client::new("http://localhost:8001/", None).await?;
+    let models = client
+        .repository_index(triton_client::inference::RepositoryIndexRequest {
+            repository_name: "".into(), // This should show us models not referenced by repo name.
+            ready: false,               // show all models, not just ready ones.
+        })
+        .await?;
+
+    // Triton Client
+    // https://github.com/octoml/triton-client-rs/blob/main/examples/repo_index.rs
+    // un-auth'd use of Triton
+    //let client = Client::new("http://localhost:8001/", None).await?;
+    let client = Client::new(triton_server_url, None).await;
+    let models = client.expect("REASON").repository_index(triton_client::inference::RepositoryIndexRequest {
+            repository_name: "".into(), // This should show us models not referenced by repo name.
+            ready: false,               // show all models, not just ready ones.
+        })
+        .await;
+
+    println!("Running models:");
+
+    for model in models.unwrap().models.iter() {
+        println!("    {:?}", model);
+    }
+
+    category_index = label_map_util.create_category_index_from_labelmap("./labels.txt", use_display_name=True)
+    if debug == 1:
+        println!("CATEGORY INDEX");
+        print(category_index)
+
+    // Setup MQTT client
+    let host = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "mqtt://localhost:1883".to_string());
+
+    println!("Connecting to the MQTT server at '{}'", host);
+
+    // Create the client
+    let cli = mqtt::AsyncClient::new(host).unwrap_or_else(|err| {
+        println!("Error creating the client: {}", err);
+        process::exit(1);
+    });
+
+     if let Err(err) = block_on(async {
+        // Connect with default options and wait for it to complete or fail
+        // The default is an MQTT v3.x connection.
+        cli.connect(None).await?;
+
+        // Create a message and publish it
+        println!("Publishing a message on the topic 'test'");
+        let msg = mqtt::Message::new("test", "Hello Rust MQTT world!", mqtt::QOS_1);
+        cli.publish(msg).await?;
+
+        // Disconnect from the broker
+        println!("Disconnecting");
+        cli.disconnect(None).await?;
+
+        Ok::<(), mqtt::Error>(())
+    }) {
+        eprintln!("{}", err);
+    }
 */
 
     // let rtsp_url = "rtsp://1701954d6d07.entrypoint.cloud.wowza.com:1935/app-m75436g0/27122ffc_stream2";
@@ -77,17 +156,22 @@ let models = client
         imencode(".jpg", &frame, &mut buffer, &opencv::core::Vector::<i32>::new()).unwrap();
 
         // Now `buffer` contains the encoded image data as a jpeg
-        println!("Encoded image size: {} bytes", buffer.len());
+        if debug > 0 {
+            println!("Encoded image size: {} bytes", buffer.len());
+        }
 
         // Encode jpeg data to text for MQTT
         let encoded = BASE64_STANDARD.encode(buffer);
-        println!("Encoded image: {} ", encoded);
+        if debug > 0 {
+            println!("Encoded image: {} ", encoded);
+        }
 
         if frame.size()?.width > 0 {
             highgui::imshow(window, &frame)?;
         }
 
-        let key = highgui::wait_key(60)?;
+        let gui_wait = 30;
+        let key = highgui::wait_key(gui_wait)?;
         if key > 0 && key != 255 {
             break;
         }
